24a67928c8103d41e090f6679b13412db9541810
Michael Chernetskyi <michael.chernetskyi@gmail.com>
2022-09-15 12:52:32 +0300

    add list gallery for js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const photoGallery = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="'${description}'"
+            />
+            </a>`;
+    })
+    .join(" ");
+gallery.insertAdjacentHTML("beforeend", ` ${photoGallery}`);
+
+const escapeCloseModal = (event) => {
+    if (event.code === 'Escape') {
+    window.removeEventListener('keydown', escapeCloseModal);
+    document.querySelector('.basicLightbox').remove();
+} return;  
+};
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const photoGallery = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="'${description}'"
+            />
+            </a>`;
+    })
+    .join(" ");
+gallery.insertAdjacentHTML("beforeend", ` ${photoGallery}`);
 
 console.log(galleryItems);
